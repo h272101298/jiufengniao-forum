c2006d6e399d4e68612460466817a3ab43caca9a
h272101298 <272101298@qq.com>
2019-12-04 17:28:05 +0800

    123456

--- app/Http/Middleware/CrossDomain.php
+++ app/Http/Middleware/CrossDomain.php
@@ -19,7 +19,7 @@ class CrossDomain
 
         //dd($allowOrigin);
         $response = $next($request);
-        dd($request->header());
+        dd($request);
         if (in_array($request->header('origin'),$allowOrigin)){
             if($request->getMethod() === 'OPTIONS'){
                 $response->header('Access-Control-Allow-Origin', $request->header('Origin'));
