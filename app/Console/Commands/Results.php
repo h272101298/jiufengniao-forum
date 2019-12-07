94d82b8b48028cc99db42b784bab99cc9e167a5b
h272101298 <272101298@qq.com>
2019-12-07 22:52:15 +0800

    1213

--- app/Console/Commands/Results.php
+++ app/Console/Commands/Results.php
@@ -39,44 +39,45 @@ class Results extends Command
     public function handle()
     {
         //
-        $url='https://api.api861861.com/pks/getLotteryPksInfo.do?issue=&lotCode=10057';
-        $results=file_get_contents($url);
-        if ($results){
-            $results=json_decode($results,true);
-            $results=$results['result']['data'];
-            $db=DB::table('results');
-            $check=$db->where('preDrawIssue',$results['preDrawIssue'])->first();
-            if(!$check){
-                $data=[
-                    'preDrawTime'=>$results['preDrawTime'],
-                    'preDrawIssue'=>$results['preDrawIssue'],
-                    'preDrawCode'=>$results['preDrawCode'],
-                    'firstNum'=>$results['firstNum'],
-                    'secondNum'=>$results['secondNum'],
-                    'thirdNum'=>$results['thirdNum'],
-                    'fourthNum'=>$results['fourthNum'],
-                    'fifthNum'=>$results['fifthNum'],
-                    'sixthNum'=>$results['sixthNum'],
-                    'seventhNum'=>$results['seventhNum'],
-                    'eighthNum'=>$results['eighthNum'],
-                    'ninthNum'=>$results['ninthNum'],
-                    'tenthNum'=>$results['tenthNum'],
-                    'sumFS'=>$results['sumFS'],
-                    'sumBigSamll'=>$results['sumBigSamll'],
-                    'sumSingleDouble'=>$results['sumSingleDouble'],
-                    'firstDT'=>$results['firstDT'],
-                    'secondDT'=>$results['secondDT'],
-                    'thirdDT'=>$results['thirdDT'],
-                    'fourthDT'=>$results['fourthDT'],
-                    'fifthDT'=>$results['fifthDT'],
-                    'groupCode'=>$results['groupCode'],
-                    'drawTime'=>$results['drawTime'],
-                    'drawIssue'=>$results['drawIssue'],
-                    'created_at'=>getDatetime(time()),
+        $url = 'https://api.api861861.com/pks/getLotteryPksInfo.do?issue=&lotCode=10057';
+        $results = file_get_contents($url);
+        if ($results) {
+            $results = json_decode($results, true);
+            $results = $results['result']['data'];
+            $db = DB::table('results');
+            $check = $db->where('preDrawIssue', $results['preDrawIssue'])->first();
+            if (!$check) {
+                $data = [
+                    'preDrawTime' => $results['preDrawTime'],
+                    'preDrawIssue' => $results['preDrawIssue'],
+                    'preDrawCode' => $results['preDrawCode'],
+                    'firstNum' => $results['firstNum'],
+                    'secondNum' => $results['secondNum'],
+                    'thirdNum' => $results['thirdNum'],
+                    'fourthNum' => $results['fourthNum'],
+                    'fifthNum' => $results['fifthNum'],
+                    'sixthNum' => $results['sixthNum'],
+                    'seventhNum' => $results['seventhNum'],
+                    'eighthNum' => $results['eighthNum'],
+                    'ninthNum' => $results['ninthNum'],
+                    'tenthNum' => $results['tenthNum'],
+                    'sumFS' => $results['sumFS'],
+                    'sumBigSamll' => $results['sumBigSamll'],
+                    'sumSingleDouble' => $results['sumSingleDouble'],
+                    'firstDT' => $results['firstDT'],
+                    'secondDT' => $results['secondDT'],
+                    'thirdDT' => $results['thirdDT'],
+                    'fourthDT' => $results['fourthDT'],
+                    'fifthDT' => $results['fifthDT'],
+                    'groupCode' => $results['groupCode'],
+                    'drawTime' => $results['drawTime'],
+                    'drawIssue' => $results['drawIssue'],
+                    'created_at' => getDatetime(time()),
                 ];
                 //dd($data);
                 DB::table('results')->insert($data);
 
+            }
         }
     }
 }
