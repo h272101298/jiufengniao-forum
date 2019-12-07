9e1fc389279a49bf7a3cbf8539aaaf711fcfb768
h272101298 <272101298@qq.com>
2019-12-07 23:22:49 +0800

    123

--- app/Console/Commands/Results.php
+++ app/Console/Commands/Results.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
-
+use App\Modules\Results\ResultsHandle;
 class Results extends Command
 {
     /**
@@ -75,7 +75,10 @@ class Results extends Command
                     'created_at' => getDatetime(time()),
                 ];
                 //dd($data);
-                DB::table('results')->insert($data);
+                $inster=DB::table('results')->insert($data);
+                if ($inster){
+                    countResults($data["preDrawIssue"]);
+                }
 
             }
         }
--- step.sh
+++ step.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-step=10 #间隔的秒时间
 
-for (( i = 0; i < 60; i=(i + step )); do
-    /use/local/php/bin/php home/www/forum/artisan schedule:run
-
-    sleep $setp
+while true; do
+  SCRIPT   /use/local/php/bin/php home/www/forum/artisan schedule:run
+  sleep 5
 done
-
-exit 0
