92b4fb69edbbdfc010d327fc3c3ca922ca5ce9b8
h272101298 <272101298@qq.com>
2019-12-09 15:45:04 +0800

    312

--- step.sh
+++ step.sh
@@ -3,7 +3,7 @@
 step=5 #间隔的秒数
 
 for (( i = 0; i < 60; i=(i+step) )); do
-    /use/bin/php /home/www/forum/artisan schedule:run
+    /usr/bin/php /home/www/forum/artisan schedule:run
     sleep $step
 done
 
