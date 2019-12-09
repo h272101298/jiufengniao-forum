a1a579076e1b8601f4882005a20f5a4f32498a2e
h272101298 <272101298@qq.com>
2019-12-09 15:01:17 +0800

    123

--- step.sh
+++ step.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 while true; do
-  SCRIPT   /use/local/php/bin/php home/www/forum/artisan schedule:run
+  SCRIPT   /use/bin/php /home/www/forum/artisan schedule:run
   sleep 5
 done
